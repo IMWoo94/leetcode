93941b757cb784220564c7b83cc9f79d7f71310a
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-01-18 16:18:01 +0900

    Time: 0 ms (100.00%), Space: 42 MB (12.21%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,12 +1,27 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int index = 0;
+//         int index = 0;
         
-        for(int num : nums){
-            if(num != val){
-                nums[index++] = num;
+//         for(int num : nums){
+//             if(num != val){
+//                 nums[index++] = num;
+//             }
+//         }
+//         return index;
+        int originSize = nums.length;
+        int answer = nums.length;
+        
+        for( int i = 0; i < answer; i++ ){
+            // 주어진 값과 배열의 값이 같은 경우
+            if( nums[i] == val ){
+                // 현재 위치의 값을 이후의 값으로 앞으로 땡겨서 가져온다.
+                for( int k = i + 1; k < originSize; k++){
+                    nums[k-1] = nums[k];
+                }
+                answer--;
+                i--;
             }
         }
-        return index;
+        return answer;
     }
 }
