7c2587f7e7ce7d117b3aabb4874f3c22d727b19f
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-01-18 17:09:17 +0900

    Time: 0 ms (100.00%), Space: 42.9 MB (69.69%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,28 +1,32 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int answer = nums.length;
         // 정렬된 배열이 주어지며, 배열의 원소 값은 중복되지 않은 정수로 이루어 집니다.
         // target 이 주어지면 target 과 일치하는 값이 있다면 해당 값의 index return
         // 일치하는 값이 없다면 target 이 들어갈만한 위치를 리턴
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] >= target){
-                return i;
+        
+        // 모든 배열을 돌면서 정렬을 하는 케이스
+//         int answer = nums.length;
+//         for(int i = 0; i < nums.length; i++){
+//             if(nums[i] >= target){
+//                 return i;
+//             }
+//         }
+        
+//         return answer;
+        
+        // 정렬된 배열이라면 이진탐색을 이용하여 시간복잡도를 줄일 수 있다. O(log n)
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
             }
         }
-        
-        // 일치하지 않는 경우 들어갈 위치를 찾아야 한다.
-        // if(nums[0] > target){
-        //     return 0;
-        // }else if(nums[nums.length - 1] < target){
-        //     return nums.length;
-        // }else{
-        //     for(int i = 0; i < nums.length -1; i++){
-        //         if(nums[i] < target && nums[i + 1] > target){
-        //             return i+1;
-        //         }
-        //     }
-        // }
-        
-        return answer;
+        return start;
     }
 }
