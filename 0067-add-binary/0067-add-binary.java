4917e8ccc4b18bf1599c2c17975ffe25d75bfff1
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-01-19 18:18:46 +0900

    Time: 1 ms (100.00%), Space: 42 MB (56.88%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -4,9 +4,33 @@ class Solution {
     public String addBinary(String a, String b) {
         
         // 2진수 인 것을 명시하여 bigInter 생성
-        BigInteger bigIntegerA = new BigInteger(a, 2);
-        BigInteger bigIntegerB = new BigInteger(b, 2);
+//         BigInteger bigIntegerA = new BigInteger(a, 2);
+//         BigInteger bigIntegerB = new BigInteger(b, 2);
         
-        return bigIntegerA.add(bigIntegerB).toString(2);
+//         return bigIntegerA.add(bigIntegerB).toString(2);
+        
+        // 
+        int aIndex = a.length() -1;
+        int bIndex = b.length() -1;
+        int sum = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        while(aIndex >= 0 || bIndex >= 0){
+            if(aIndex >= 0){
+                sum += a.charAt(aIndex--) - '0';
+            }
+            if(bIndex >= 0){
+                sum += b.charAt(bIndex--) - '0';
+            }
+            
+            sb.append(sum%2);
+            sum /= 2;
+        }
+        
+        if(sum == 1){
+            sb.append(sum);
+        }
+        String answer = sb.reverse().toString();
+        return answer;
     }
 }
