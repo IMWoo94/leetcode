0638f5a71c579a555d0c57e148322d4dab98fc4d
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-01-27 16:21:08 +0900

    Time: 0 ms (100.00%), Space: 42.6 MB (96.65%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -23,12 +23,15 @@ class Solution {
     }
     
     public boolean dfs(TreeNode root, int targetSum){
+        if(root == null){
+            return false;
+        }
         if (root.left == null && root.right == null){
             return targetSum == root.val;
         }
         
-        boolean leftSum = hasPathSum(root.left, targetSum - root.val);
-        boolean rightSum = hasPathSum(root.right, targetSum - root.val);
+        boolean leftSum = dfs(root.left, targetSum - root.val);
+        boolean rightSum = dfs(root.right, targetSum - root.val);
 
         return leftSum || rightSum;
     }
