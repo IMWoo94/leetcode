0a8724d7f60e9e47436dcde8950b8e23d71d9518
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-01-27 15:59:03 +0900

    Time: 205 ms (98.91%), Space: 43.3 MB (92.61%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -4,23 +4,52 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+import queue
+
 class Solution:
     # DFS
+#     def minDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+        
+#         left = self.minDepth(root.left)
+#         right = self.minDepth(root.right)
+        
+#         if not left and not right:
+#             return 1
+        
+#         if not left:
+#             return 1 + right;
+        
+#         if not right:
+#             return 1 + left;
+        
+#         return min(left, right) + 1
+    
+    # BFS
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
+        if root is None:
             return 0
         
-        left = self.minDepth(root.left)
-        right = self.minDepth(root.right)
+        q = queue.Queue()
+        q.put(root)
+        depth = 0
         
-        if not left and not right:
-            return 1
+        while not q.empty():
+            size = q.qsize()
+            depth += 1
+            for num in range(size):
+                node = q.get()
+                if node.left is None and node.right is None:
+                    return depth
+                
+                if node.right:
+                    q.put(node.right)
+                    
+                if node.left:
+                    q.put(node.left)
+            
+        return depth
         
-        if not left:
-            return 1 + right;
         
-        if not right:
-            return 1 + left;
-        
-        
-        return min(left, right) + 1
