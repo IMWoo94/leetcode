d9aaabbfe934527dc342e937e65bde17d51d3539
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-01-31 18:08:57 +0900

    Time: 6 ms (70.21%), Space: 44.6 MB (45.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -20,21 +20,41 @@ class Solution {
         
 //         return maxLength;
         
-        int n = s.length();
-        int maxLength = 0;
-        Map<Character, Integer> charMap = new HashMap<>();
-        int left = 0;
+//         int n = s.length();
+//         int maxLength = 0;
+//         Map<Character, Integer> charMap = new HashMap<>();
+//         int left = 0;
         
-        for (int right = 0; right < n; right++) {
-            if (!charMap.containsKey(s.charAt(right)) || charMap.get(s.charAt(right)) < left) {
-                charMap.put(s.charAt(right), right);
-                maxLength = Math.max(maxLength, right - left + 1);
-            } else {
-                left = charMap.get(s.charAt(right)) + 1;
-                charMap.put(s.charAt(right), right);
+//         for (int right = 0; right < n; right++) {
+//             if (!charMap.containsKey(s.charAt(right)) || charMap.get(s.charAt(right)) < left) {
+//                 charMap.put(s.charAt(right), right);
+//                 maxLength = Math.max(maxLength, right - left + 1);
+//             } else {
+//                 left = charMap.get(s.charAt(right)) + 1;
+//                 charMap.put(s.charAt(right), right);
+//             }
+//         }
+        
+//         return maxLength;
+        
+        int prev = 0;
+        int idx = 0;
+        int max = 0;
+        Set<Character> set = new HashSet<>();
+        
+        while(idx < s.length()) {
+            char curn = s.charAt(idx);
+            if(!set.contains(curn)) {
+                set.add(curn);
+                idx++;
+                max = Math.max(max, set.size());
+            }
+            else {
+                set.remove(s.charAt(prev));
+                prev++;
             }
         }
-        
-        return maxLength;
+        return max;
+
     }
 }
