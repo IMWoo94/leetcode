68155cdc71e1b363392dbf53317940b82c9ff178
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-03-30 21:35:31 +0900

    Time: 2 ms (98.99%), Space: 42.5 MB (96.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // 소문자 처리
-		String lowerString = s.toLowerCase();
-		StringBuilder sb = new StringBuilder();
-		for(char c : lowerString.toCharArray()){
-			// 영숫자 확인
-			if(Character.isLetterOrDigit(c)) sb.append(c);
+        // 처음 끝 단어에 대해서 영숫자라면 서로의 일치하는지 확인하는 방법
+		int start = 0;
+		int end = s.length() - 1;
+		// 서로 중앙으로 이동하면서 나가다 겹치는 지점에 도달하면 종료
+		while (start < end) {
+			// 영숫자 판별
+			if (!Character.isLetterOrDigit(s.charAt(start))) {
+				start++;
+			} else if (!Character.isLetterOrDigit(s.charAt(end))) {
+				end--;
+			} else {
+				// 둘다 유효한 문자인 경우 소문자로 변경 비교
+				if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+					return false;
+				}
+
+				start++;
+				end--;
+			}
 		}
-
-		String a = sb.toString();
-		String b = sb.reverse().toString();
-
-		return a.equals(b);
+		return true;
     }
 }
