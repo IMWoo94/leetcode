e8072797793d5fb4429cc2cfc312462938ed2870
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-01 18:24:59 +0900

    Time: 3 ms (68.19%), Space: 45 MB (28.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,15 +5,13 @@ class Solution {
 		Map<Integer, Integer> map = new HashMap<>();
 
 		for (int i = 0; i < nums.length; i++) {
-			map.put(nums[i], i);
-		}
-
-		// target 에서 현재 수를 뺀 값이 key 로 존재한다면 2 수의 합으로 target 성립
-		for (int i = 0; i < nums.length; i++) {
-			int key = target - nums[i];
-			if (map.containsKey(key) && map.get(key) != i) {
-				return new int[] {i, map.get(key)};
+			// target 에서 num 을 뺸 값이 존재하면 정답으로 리턴
+			if (map.containsKey(target - nums[i])) {
+				return new int[] {map.get(target - nums[i]), i};
 			}
+
+			// 정답이 아니므로 현재 인덱스 저장
+			map.put(nums[i], i);
 		}
 
 		return null;
