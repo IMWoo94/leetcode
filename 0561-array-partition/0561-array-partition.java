152b096a35dd1ec9d858774b07ea9a5757adbb9d
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-02 17:06:35 +0900

    Time: 12 ms (96.99%), Space: 46.9 MB (13.40%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -5,7 +5,10 @@ class Solution {
 
 		int sum = 0;
 		for (int i = 0; i < nums.length; i = i + 2) {
-			sum += Math.min(nums[i], nums[i + 1]);
+			// sum += Math.min(nums[i], nums[i + 1]);
+            // 짝수 인덱스만 바라보아도 가능 단, 오름차순
+            // 내림차순에서는 홀수 인덱스를 바라보아야한다.
+            sum += nums[i];
 		}
 
 		return sum;
