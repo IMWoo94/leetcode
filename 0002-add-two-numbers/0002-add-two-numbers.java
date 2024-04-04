452e0a7ded7dd9c8e120befb49a82710b988da20
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-04 19:42:14 +0900

    Time: 1 ms (100.00%), Space: 44 MB (88.29%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,63 +8,38 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.math.BigInteger;
-
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		ListNode reversedL1 = reverse(l1);
-		ListNode reversedL2 = reverse(l2);
-
-		// 임의 정밀도 정수형으로 변환하여 더하기 연산 진행
-		BigInteger sum = toBigInt(reversedL1).add(toBigInt(reversedL2));
-
-		// 결과를 다시 역순으로
-		return toReversedLinkedList(sum);
-
-	}
-
-	// 연결 리스트 역순 정렬
-	public ListNode reverse(ListNode root) {
+		ListNode root = new ListNode(0);
 		ListNode node = root;
-		ListNode prev = null;
 
-		while (node != null) {
-			ListNode next = node.next;
-			node.next = prev;
+		int remainder = 0;
+		int carry = 0;
+		int sum = 0;
+		while (l1 != null || l2 != null || carry != 0) {
+			sum = 0;
 
-			prev = node;
-			node = next;
-		}
-		return prev;
-	}
+			// 첫번째 연결 리스트 합산
+			if (l1 != null) {
+				sum += l1.val;
+				l1 = l1.next;
+			}
+			// 두번째 연결 리스트 합산
+			if (l2 != null) {
+				sum += l2.val;
+				l2 = l2.next;
+			}
+			// 노드의 값으로 사용할 올림 처리 나머지 계산
+			remainder = (sum + carry) % 10;
+			// 올림 값
+			carry = (sum + carry) / 10;
 
-	// 연결 리스트의 값 이어서 정수로 변환
-	public BigInteger toBigInt(ListNode node) {
-		String val = "";
+			node.next = new ListNode(remainder);
 
-		while (node != null) {
-			val += node.val;
 			node = node.next;
 		}
-		return new BigInteger(val);
-	}
 
-	// 주어진 정수를 연결리스트 역순으로 처리
-	public ListNode toReversedLinkedList(BigInteger val) {
-		ListNode prev = null;
-		ListNode node = null;
-
-		// 정수형을 문자로 바꾼 다음 문자 배열로 전환하여 한 글자씩 순회
-		for (char c : String.valueOf(val).toCharArray()) {
-			// 한글자씩 숫자로 변환하여 노드 생성
-			node = new ListNode(Character.getNumericValue(c));
-			// 최종 결과는 뒤집어야 하므로 현재 노드의 다음으로 이전 노드 지전
-			node.next = prev;
-			// 이전 노드는 현재 노드 지정
-			prev = node;
-		}
-
-		return node;
+		return root.next;
 	}
 
 }
