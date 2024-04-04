c15834ceff74ff6b9434d4ca346b669025b73203
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-04 21:58:20 +0900

    Time: 168 ms (74.61%), Space: 37 MB (65.80%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.kt
@@ -0,0 +1,30 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun oddEvenList(head: ListNode?): ListNode? {
+        if (head == null)
+            return null
+
+        var odd = head
+        var even = head.next
+        var evenHead = even
+
+        while (even?.next != null) {
+            odd!!.next = odd.next!!.next
+            even.next = even.next!!.next
+
+            odd = odd!!.next
+            even = even!!.next
+        }
+
+        odd!!.next = evenHead
+        return head
+    }
+}
