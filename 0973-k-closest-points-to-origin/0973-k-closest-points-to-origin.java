d834b0aad8c2dc5479a3b95f07b1d1e9e9e10dea
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-08 19:58:34 +0900

    Time: 27 ms (66.64%), Space: 51.9 MB (60.98%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -14,7 +14,8 @@ class Solution {
 
 		// 좌표 목록으로 거리와 좌표 정보를 Point 객체로 생성하고 우선순위 큐에 넣기
 		for (int[] point : points) {
-			double dis = Math.sqrt(Math.pow((0 - point[0]), 2) + Math.pow((0 - point[1]), 2));
+            // 거리를 계산하는 값에서 굳이 Math.sqrt 제곱근 처리를 하지 않아도 결과는 동일하다. 그러므로 불필요한 작업을 제외하면서 좀더 나은 성능을 기대해볼만한다.
+			double dis = Math.pow((0 - point[0]), 2) + Math.pow((0 - point[1]), 2);
 			pq.add(new Point(dis, point));
 		}
 
