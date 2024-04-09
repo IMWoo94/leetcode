d28eea6a8054d0bd8f013c5af9823d2a9bbabeb5
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-09 19:28:36 +0900

    Time: 1 ms (55.97%), Space: 41.8 MB (37.90%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,15 +1,16 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-		Map<Character, Integer> map = new HashMap<>();
-		for (char c : stones.toCharArray()) {
-			map.put(c, map.getOrDefault(c, 0) + 1);
-		}
-
 		int answer = 0;
-		for (char c : jewels.toCharArray()) {
-			answer += map.getOrDefault(c, 0);
-		}
 
+		Set<Character> set = new HashSet<>();
+
+		for (char c : jewels.toCharArray()) {
+			set.add(c);
+		}
+		for (char c : stones.toCharArray()) {
+			if (set.contains(c))
+				answer++;
+		}
 		return answer;
     }
 }
