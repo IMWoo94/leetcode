999bbf5ac9b437152177d48b8e39d93a549a6525
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-10 19:46:27 +0900

    Time: 5 ms (87.35%), Space: 44.6 MB (41.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -37,24 +37,42 @@ class Solution {
         
 //         return maxLength;
         
-        int prev = 0;
-        int idx = 0;
-        int max = 0;
-        Set<Character> set = new HashSet<>();
+//         int prev = 0;
+//         int idx = 0;
+//         int max = 0;
+//         Set<Character> set = new HashSet<>();
         
-        while(idx < s.length()) {
-            char curn = s.charAt(idx);
-            if(!set.contains(curn)) {
-                set.add(curn);
-                idx++;
-                max = Math.max(max, set.size());
-            }
-            else {
-                set.remove(s.charAt(prev));
-                prev++;
-            }
-        }
-        return max;
+//         while(idx < s.length()) {
+//             char curn = s.charAt(idx);
+//             if(!set.contains(curn)) {
+//                 set.add(curn);
+//                 idx++;
+//                 max = Math.max(max, set.size());
+//             }
+//             else {
+//                 set.remove(s.charAt(prev));
+//                 prev++;
+//             }
+//         }
+//         return max;
+        
+        int left = 0;
+		int right = 0;
+		int max = 0;
+		// 사용 된 단어
+		Map<Character, Integer> word = new HashMap();
+
+		for (char c : s.toCharArray()) {
+			if (word.containsKey(c) && left <= word.get(c)) {
+				left = word.get(c) + 1;
+			} else {
+				max = Math.max(max, right - left + 1);
+			}
+			word.put(c, right);
+			right++;
+		}
+
+		return max;
 
     }
 }
