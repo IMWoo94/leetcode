a2ac409199866e01d1c0b5970c34c47748599cc1
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-15 00:52:14 +0900

    Time: 71 ms (5.07%), Space: 167.9 MB (5.01%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,7 +1,8 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
 		List<List<Integer>> answer = new ArrayList<>();
-		recursiveDFS(answer, new LinkedList<>(), n, 1, k);
+		// recursiveDFS(answer, new LinkedList<>(), n, 1, k);
+        iterativeDFS(answer, n, k);
         return answer;
     }
     
@@ -18,6 +19,27 @@ class Solution {
 			recursiveDFS(results, elements, n, i + 1, k - 1);
 			// 돌아온 후 현재 엘리먼트 제거
 			elements.removeLast();
+		}
+	}
+    
+	public void iterativeDFS(List<List<Integer>> results, int n, int k) {
+		Deque<LinkedList<Integer>> stack = new ArrayDeque<>();
+
+        stack.add(new LinkedList<>());
+		while (!stack.isEmpty()) {
+			LinkedList<Integer> current = stack.pop();
+
+			if (current.size() == k) {
+				results.add(current);
+				continue;
+			}
+
+			int start = current.isEmpty() ? 1 : current.get(current.size() - 1) + 1;
+			for (int i = start; i <= n; i++) {
+				current.add(i);
+				stack.add(new LinkedList<>(current));
+				current.removeLast();
+			}
 		}
 	}
 }
