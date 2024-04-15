b1160859674aa53130398e27e4115d51f28a56fa
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-15 16:26:50 +0900

    Time: 15 ms (5.57%), Space: 45.3 MB (5.48%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,7 +2,8 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 		List<List<Integer>> results = new ArrayList<>();
 		List<Integer> elements = Arrays.stream(candidates).boxed().collect(Collectors.toList());
-		recursiveDFS(results, elements, new ArrayDeque<>(), target, 0);
+		// recursiveDFS(results, elements, new ArrayDeque<>(), target, 0);
+        iterativeDFS(results, elements, target);
 		return results;
     }
     
@@ -20,6 +21,44 @@ class Solution {
 			comb.add(elements.get(i));
 			recursiveDFS(results, elements, comb, target - elements.get(i), i);
 			comb.removeLast();
+		}
+	}
+    
+	public void iterativeDFS(List<List<Integer>> results, List<Integer> elements, int target) {
+		Deque<combSum> stack = new ArrayDeque<>();
+		stack.add(new combSum(new LinkedList<>(), 0, 0));
+
+		while (!stack.isEmpty()) {
+			combSum comb = stack.pop();
+
+			if (comb.sum == target) {
+				results.add(comb.list);
+				continue;
+			}
+			if (comb.sum > target) {
+				continue;
+			}
+
+			for (int i = comb.index; i < elements.size(); i++) {
+				comb.list.add(elements.get(i));
+				comb.sum += elements.get(i);
+				stack.push(new combSum(new LinkedList<>(comb.list), i, comb.sum));
+				comb.sum -= elements.get(i);
+				comb.list.removeLast();
+			}
+		}
+
+	}
+
+	class combSum {
+		public LinkedList<Integer> list;
+		public int index;
+		public int sum;
+
+		public combSum(LinkedList<Integer> list, int index, int sum) {
+			this.list = list;
+			this.index = index;
+			this.sum = sum;
 		}
 	}
 }
