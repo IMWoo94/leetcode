cbd723abd60188ccd9b377ea6d5a0293a188b8fb
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-15 17:11:57 +0900

    Time: 165 ms (96.53%), Space: 37.2 MB (80.21%) - LeetHub

---
+++ 0078-subsets/0078-subsets.kt
@@ -0,0 +1,20 @@
+import java.util.ArrayDeque
+
+class Solution {
+    fun subsets(nums: IntArray): List<List<Int>> {
+        val results: MutableList<List<Int>> = mutableListOf()
+
+        fun dfs(index: Int, path: Deque<Int>) {
+            results.add(ArrayList(path))
+
+            for (i in index until nums.size) {
+                path.add(nums[i])
+                dfs(i + 1, path)
+                path.removeLast()
+            }
+        }
+
+        dfs(0, ArrayDeque())
+        return results
+    }
+}
