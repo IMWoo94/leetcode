68611e95dc57cd2bfb9d9ca954d71e7bde356b88
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-15 17:06:55 +0900

    Time: 3 ms (5.53%), Space: 43.1 MB (6.54%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,7 +2,8 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
 		List<List<Integer>> results = new ArrayList<>();
 		List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
-		recursiveDFS(results, list, 0, new ArrayDeque<>());
+		// recursiveDFS(results, list, 0, new ArrayDeque<>());
+        iterativeDFS(results, list);
 		return results;
     }
     
@@ -12,6 +13,35 @@ class Solution {
 			path.add(nums.get(i));
 			recursiveDFS(results, nums, i + 1, path);
 			path.removeLast();
+		}
+	}
+    
+	public void iterativeDFS(List<List<Integer>> results, List<Integer> nums) {
+		Deque<Pair> stack = new ArrayDeque<>();
+
+		stack.push(new Pair(0, new LinkedList<>()));
+
+		while (!stack.isEmpty()) {
+
+			Pair pair = stack.pop();
+
+			results.add(new ArrayList<>(pair.path));
+
+			for (int i = pair.index; i < nums.size(); i++) {
+				pair.path.add(nums.get(i));
+				stack.push(new Pair(i + 1, new LinkedList<>(pair.path)));
+				pair.path.removeLast();
+			}
+		}
+	}
+
+	class Pair {
+		public int index;
+		public LinkedList<Integer> path;
+
+		public Pair(int index, LinkedList<Integer> path) {
+			this.index = index;
+			this.path = path;
 		}
 	}
 }
