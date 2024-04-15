75f20a922eb5cf736fb8d71d80154a7812d70207
Lee SangMin <75981576+IMWoo94@users.noreply.github.com>
2024-04-15 16:42:52 +0900

    Time: 191 ms (99.00%), Space: 38.1 MB (89.97%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.kt
@@ -0,0 +1,26 @@
+import java.util.ArrayDeque
+
+class Solution {
+    fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
+        val results: MutableList<List<Int>> = mutableListOf()
+
+        fun dfs(target: Int, index: Int, path: Deque<Int>) {
+            if (target < 0) {
+                return
+            }
+            if (target == 0) {
+                results.add(ArrayList(path))
+                return
+            }
+
+            for (i in index until candidates.size) {
+                path.add(candidates[i])
+                dfs(target - candidates[i], i, path)
+                path.removeLast()
+            }
+        }
+
+        dfs(target, 0, ArrayDeque())
+        return results
+    }
+}
